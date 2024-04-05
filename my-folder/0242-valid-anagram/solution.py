aafd13dd79e0b6cc90563ef72c6bcfad32dba3a4
Anushka Verma <123035188+anushkav03@users.noreply.github.com>
2024-04-05 20:11:19 +0000

    [LeetCode Sync] Runtime - 50 ms (5.01%), Memory - 14.5 MB (9.33%)

---
+++ my-folder/0242-valid-anagram/solution.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        
+        if len(s) != len(t):
+            return False
+
+        sdict = {}
+        tdict = {} 
+
+        for i in range(0, len(s)):
+            if s[i] in sdict:
+                sdict[s[i]] += 1
+            else:
+                sdict[s[i]] = 1 
+
+            if t[i] in tdict:
+                tdict[t[i]] += 1
+            else:
+                tdict[t[i]] = 1
+
+        if sdict==tdict:
+            return True
+        else:
+            return False
+        """
+        if len(s) != len(t):
+            return False
+
+        s = "".join(sorted(s))   
+        t = "".join(sorted(t))
+
+        for i in range(0, len(s)):
+            if s[i] != t[i]:
+                return False
+        return True
+
