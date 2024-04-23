936536dea5b49624945ffeb36ac0de0bc223ffc9
Anushka Verma <123035188+anushkav03@users.noreply.github.com>
2024-04-23 02:57:40 +0000

    [LeetCode Sync] Runtime - 76 ms (5.07%), Memory - 19.3 MB (100.00%)

---
+++ my-folder/0049-group-anagrams/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = {}
+        for word in strs:
+            sortedword = ''.join(sorted(word))
+            if sortedword in dict:
+                dict[sortedword].append(word)
+            else:
+                dict[sortedword] = [word]
+        
+        return list(dict.values())
+        
