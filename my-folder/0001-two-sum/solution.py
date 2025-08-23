66c9eb3267299f5b8987252af10ffacca7ebbe1b
Anushka Verma <123035188+anushkav03@users.noreply.github.com>
2025-08-23 00:28:20 +0000

    [LeetCode Sync] Runtime - 3 ms (53.47%), Memory - 18.7 MB (100.00%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,12 +1,36 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(0, len(nums)):
-            for j in range(0, len(nums)):
-                if i!=j:
-                    if nums[i] + nums[j] == target:
-                        return [i, j]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ## OPTIMIZATION 1 ##
+        numindex = {}
+        for i in range(len(nums)):
+            # check if target in index
+            subtarget = target - nums[i]
+            if subtarget in numindex and numindex[subtarget] != i:
+                # return answer
+                return [numindex[subtarget], i]
+
+            # add num to index
+            if nums[i] in numindex:
+                pass
+            else:
+                numindex[nums[i]] = i # num : index
+        return []
+            
+
+        ## BRUTE FORCE: Time O(n^2) Space O(n) ##
+        # #sortednums = nums[:]
+        # #sortednums.sort() # make a copy and sort nums
+        # for i in range(len(nums)): # pointer 1
+        #     subtarget = target - nums[i]
+
+        #     for j in range(len(nums)-1, -1, -1): # pointer 2
+        #         if i != j:
+        #             if nums[j] < subtarget:
+        #                 continue
+        #             if nums[j] == subtarget:
+        #                 #return [nums.index(sortednums[i]), nums.index(sortednums[j])]
+        #                 return [i, j]
+        # return []
+            
+
+        
