96b0d62bde42461160d9ec6bc2dc8353293f24c9
Anushka Verma <123035188+anushkav03@users.noreply.github.com>
2025-09-01 01:17:56 +0000

    [LeetCode Sync] Runtime - 53 ms (58.67%), Memory - 53.7 MB (81.18%)

---
+++ my-folder/2859-add-two-promises/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    num1 = await promise1.then()
+    num2 = await promise2.then()
+
+    //  async func return values get wrapped in a promise
+    return num1 + num2 
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
