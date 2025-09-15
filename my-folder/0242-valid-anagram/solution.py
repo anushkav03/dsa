28c413e96ec1482495b39eaed740a684defa1675
Anushka Verma <123035188+anushkav03@users.noreply.github.com>
2025-09-15 06:11:23 +0000

    [LeetCode Sync] Runtime - 17 ms (21.08%), Memory - 18.6 MB (100.00%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,41 +1,4 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
         
-        if len(s) != len(t):
-            return False
-
-        sdict = {}
-        tdict = {} 
-
-        for i in range(0, len(s)):
-            if s[i] in sdict:
-                sdict[s[i]] += 1
-            else:
-                sdict[s[i]] = 1 
-
-            if t[i] in tdict:
-                tdict[t[i]] += 1
-            else:
-                tdict[t[i]] = 1
-
-        if sdict==tdict:
-            return True
-        else:
-            return False
-        """
-        if len(s) != len(t):
-            return False
-
-        s = "".join(sorted(s))   
-        t = "".join(sorted(t))
-
-        #for i in range(0, len(s)):
-        #    if s[i] != t[i]:
-        #        return False
-        
-        return s==t
-
