192bca41f30529304b86bd06af2f11cbb602289b
Anushka Verma <123035188+anushkav03@users.noreply.github.com>
2025-10-23 20:07:56 +0000

    [LeetCode Sync] Runtime - 17 ms (52.31%), Memory - 13.8 MB (27.51%)

--- my-folder/0125-valid-palindrome/solution.py
+++ my-folder/0125-valid-palindrome/solution.py
@@ -4,19 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        if len(s) <= 1:
-            return True 
+        s = s.lower() 
+        s = "".join([x for x in s if x in "0123456789qwertyuiopasdfghjklzxcvbnm"])
 
-        s = s.lower()
-        s = re.sub(r'[^0-9a-z]+', '', s)
-
-        # 0 1 2 3
-        # 3 2 1 0
-
-        for i in range(0, len(s)-1):
-            if len(s)-i-1 < i:
-                return True
-            if s[i] != s[len(s)-i-1]:
-                return False
-        return True
-        
+        return s == s[::-1]
