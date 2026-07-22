aa5cca8a6bd30a9ba70fdbdc139c13daa9300b6d
Anushka Verma <123035188+anushkav03@users.noreply.github.com>
2026-07-22 21:48:53 +0000

    [LeetCode Sync] Runtime - 47 ms (92.00%), Memory - 29.9 MB (5.89%)

--- my-folder/0011-container-with-most-water/solution.py
+++ my-folder/0011-container-with-most-water/solution.py
@@ -1,20 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # logic: left pointer starts at left end, right pointer starts at right
-        # of the two, whichever is shorter, move that one forward towards centre 
-        # remember goal is to calculate max height => moving shorter height bar is
-        # in a sense discarding it 
-        left = 0
-        right = len(height) - 1
-        maxheight = 0
-        while right > left:
-            curr_height = (right-left) * min(height[left], height[right])
-            maxheight = max(maxheight, curr_height)
-
-            if height[left] >= height[right]:
-                right -= 1
+        max_area = 0
+        l = 0
+        r = len(height) - 1
+        while r > l:
+            area = (r - l) * min(height[r], height[l])
+            max_area = max(max_area, area)
+            # print(r, l, "area: ", area, " max area: ", max_area)
+            if height[r] <= height[l]:
+                r -= 1
             else:
-                left += 1
-        return maxheight
-
-
+                l += 1
+        return max_area
+        
